f56ba8111424f07e5b9c8dcf156ba3f36b04d1a0
abeer-sohail-invx <135139762+abeer-sohail-invx@users.noreply.github.com>
2023-06-14 11:10:54 +0500

    Fixed node_modules being replaced by home files, by removing backend volumes.

--- frontend/src/components/EntryDetails.js
+++ frontend/src/components/EntryDetails.js
@@ -6,11 +6,11 @@ const EntryDetails = ({ entry }) => {
         <p><strong>Name: </strong>{entry.name}</p>
         <p><strong>Longitude: </strong>{entry.longitude} </p>
         <p><strong>Latitude: </strong>{entry.latitude} </p>
-        <p><strong>Volume consumed: </strong>{entry.volume} m3</p>
+        <p><strong>Volume consumed: </strong>{entry.volume} m^3</p>
         <p><strong>Flow: </strong>{entry.flow} m^3/s</p>
         <p><strong>Battery level: </strong>{entry.battery} %</p>
         <p><strong>Current: </strong>{entry.current} mA</p>
-        <p>{entry.createdAt}</p>
+        <p>Timestamp: {entry.createdAt}</p>
       </div>
     )
   }
